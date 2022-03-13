db98e2c7ac3454f790522248e4caf2683483b8c1
박규민 <pkm229125@naver.com>
2022-03-13 21:23:11 +0900

    first

---
+++ Youtube-java01/src/hello.java
@@ -0,0 +1,33 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//ctrl+shif+L	단축키 전체 목록보기
+		//ctrl+D	한 줄 삭제
+		//ctrl+alt+down		행단위 복사 붙여넣기
+		//alt+shift+A 	멀티컬러 편집
+		//alt+up,down	행단위 이동
+		//tab	들여 쓰기 			shift+tab	들여쓰기취소
+		//ctrl+i	자동 들여쓰기
+		//ctrl+/	주석(토글)
+		//    /*           */		이런 방법도 비쥬얼스튜디오처럼 있음(여러줄 주석)
+		//ctrl+space	자동완성!	ex)sysout
+		
+		//print()=> 출력후에 줄바꿈을 안함
+		//println()=> 출력후에 줄바꿈을 함
+		
+		System.out.println(5+3);    //덧셈
+		System.out.println(5-3);	//빼기
+		System.out.println(5*3);	//곱셈
+		System.out.println(5/3.0);	//나눗셈
+		
+		
+		
+		
+		
+		
+	}
+
+}
