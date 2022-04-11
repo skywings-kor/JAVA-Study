c2e35a3b96741dfa4c47003d1144d7822b58e1f9
박규민 <pkm229125@naver.com>
2022-04-11 17:56:12 +0900

    4.11-2차

--- 2022_javastudy/src/study0411/Car.java
+++ 2022_javastudy/src/study0411/Car.java
@@ -4,11 +4,30 @@ package study0411;
 public class Car {
 	String vender,model,owner;
 	
-	public Car(String a,String b,String c)
+	public Car(String a)
 	{
 		this.vender=a;
+		System.out.println("1번 생성자");
+	}
+	
+	
+	public Car(String a,String b)		//2개 값 전달해줄때 실행되는 부분
+	{
+		this(a);		//생성자 호출구문은 반드시 첫 라인에서 와야한다
+//		this.vender=a;
 		this.model=b;
+		System.out.println("2번 생성자");
+	}
+	
+	
+	
+	public Car(String a,String b,String c)	//3개 값 전달해줄때 실행되는 부분(메소드의 오버로딩 이다)
+	{
+		this(a,b);
+//		this.vender=a;
+//		this.model=b;
 		this.owner=c;
+		System.out.println("3번 생성자");
 	}
 	
 	
@@ -25,6 +44,24 @@ public class Car {
 	{
 		System.out.printf("생산자:%s, 모델:%s, 소유자:%s \n",this.vender,this.model,this.owner);
 	}
+	
+	//()안에 있는 것에 따라 같은 함수 이름이더라도 해당 부분을 찾아가서 실행함
+//	public void printValue(Object object)
+//	{
+//		System.out.println(object);
+//	}			
+	public void printValue(char value)
+	{
+		System.out.println(value);
+	}			
+	public void printValue(String value)
+	{
+		System.out.println(value);
+	}			
+	public void printValue(double value)
+	{
+		System.out.println(value);
+	}			
 	
 	
 	
--- 2022_javastudy/src/study0411/Myclass.java
+++ 2022_javastudy/src/study0411/Myclass.java
@@ -1,6 +1,7 @@
 package study0411;
 
-import java.util.Scanner;
+import java.util.*;
+
 public class Myclass {
 
 	public static void main(String[] args) {
@@ -9,8 +10,32 @@ public class Myclass {
 		
 		mycar1.carInfo();
 		
-		Car mycar2=new Car("기아자동차","EV6","???");
-		mycar2.carInfo();
+		//Car mycar2=new Car("기아자동차","EV6");
+		//mycar2.carInfo();
+		
+//		ArrayList alist=new ArrayList();
+//		
+//		alist.add(10);
+//		alist.add("홍길동");
+//		alist.add(42.3456);
+//		alist.add(100);
+//		alist.add("둘리");
+//		
+//		alist.add(10000);
+//		alist.add("홍길동");
+//		alist.add(94.256);
+//		alist.add(100);
+//		alist.add("둘리");
+		
+//		for(int i=0 ; i<alist.size();i++)
+//		{
+//			mycar1.printValue(alist.get(i));
+//		}
+		
+		
+		
+		
+//		
 	}
 	
 }
