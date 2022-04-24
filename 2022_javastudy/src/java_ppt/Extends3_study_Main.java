0843a779370cf7b1560f37c9cd3881b668a56e72
박규민 <pkm229125@naver.com>
2022-04-25 01:06:12 +0900

    자바 추가 공부

---
+++ 2022_javastudy/src/java_ppt/Extends3_study_Main.java
@@ -0,0 +1,15 @@
+package java_ppt;
+
+public class Extends3_study_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Extends3_study_school st1=new Extends3_study_school("testid","testpw","박규민","이세카이","20-2929",292929);
+		Extends3_study_studentnumber st2=new Extends3_study_studentnumber("testid2","testpw2","이해준","일본","지스쿨",23);
+		st1.printstnum();
+		System.out.println("");
+		st2.stnumprint();
+		
+	}
+
+}
