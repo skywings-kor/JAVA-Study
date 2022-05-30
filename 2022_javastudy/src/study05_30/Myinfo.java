1091781050d317514eb8bc1bc88cb5d6310a05c6
박규민 <pkm229125@naver.com>
2022-05-30 22:55:16 +0900

    05.30study

---
+++ 2022_javastudy/src/study05_30/Myinfo.java
@@ -0,0 +1,13 @@
+package study05_30;
+
+public class Myinfo {
+	
+	public void print()
+	{
+		System.out.println("class MyInfo");
+		
+		
+	}
+	
+	
+}
---
+++ 2022_javastudy/src/study05_30_yourpackage/sub/Yourinfo.java
@@ -0,0 +1,9 @@
+package study05_30_yourpackage.sub;
+
+public class Yourinfo {
+
+	public void print()
+	{
+		System.out.println("¾Ó±â¸ð¸µ");
+	}
+}
